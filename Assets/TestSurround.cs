96c00ca2ee2dd401f3aecd71b1e0bb8645400712
Koki Ibukuro <koki.ibukuro@gmail.com>
2017-08-08 21:06:55 +0900

    Initial commit

---
+++ Assets/TestSurround.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class TestSurround : MonoBehaviour
+{
+    public AudioClip[] clips;
+    public AudioSpeakerMode speakerMode;
+
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        AudioSettings.speakerMode = speakerMode;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnChannelSelect(int index)
+    {
+        Debug.LogFormat("On index select : {0}", index);
+        if (index == 0)
+        {
+            audioSource.Stop();
+        }
+        else
+        {
+            audioSource.clip = clips[index - 1];
+            audioSource.Play();
+        }
+    }
+
+}
